14b0ce721c82d280c5d580334deb1f169e050640
SuxyShellSnarf <mitchellparker@oakland.edu>
2019-03-28 16:06:56 -0400

    Update node.sql

--- sql/node.sql
+++ sql/node.sql
@@ -4,7 +4,7 @@ user_id int unsigned auto_increment primary key
 
 create table messages (
 message_id int unsigned auto_increment primary key,
-message varchar(255);
+message varchar(255),
 user_id int unsigned,
 foreign key (user_id) references user(user_id);
 );
