60061bd1bf948208a0e5f630a5a1681f678bf975
Mitchell Parker <mitchellparker@oakland.edu>
2019-03-29 00:51:17 -0400

    Adding comments

--- server.php
+++ server.php
@@ -1,4 +1,5 @@
 <?php
+//First initialize the database information
 $host = "34.73.219.7";
 $port = 8001;
 
@@ -8,16 +9,14 @@ set_time_limit(0);
 
 ob_implicit_flush();
 
+//Create the original socket
 $socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP) or die("Could not create socket\n");
 socket_set_option($socket, SOL_SOCKET, SO_REUSEADDR, 1);
 socket_bind($socket, $host, $port) or die("Could not bind to socket\n");
 socket_listen($socket, 3) or die("Could not set up socket listener");
-$clients = array($socket);
-$servers = array(
-    "" => "",
-    "" => "",
 
-);
+//This will allow us to handle multiple clients at the same time
+$clients = array($socket);
 
 while(true) {
      $read = $clients;
@@ -28,6 +27,7 @@ while(true) {
          continue;
      }
 
+     //If there is a new socket, declare it
      if (in_array($socket, $read)) {
          $clients[] = $newsocket = socket_accept($socket);
          socket_getpeername($newsocket, $ip, $host);
@@ -37,8 +37,11 @@ while(true) {
          unset($read[$key]);
     }
 
+     //Loop through all of the available sockets
     foreach ($read as $read_socket) {
         $data = @socket_read($read_socket, 4096, PHP_BINARY_READ);
+
+        //Iterate through all of the connected lines and determine if any have been disconnected and remove them
         if ($data === false) {
              $key = array_search($read_socket, $clients);
              unset($clients[$key]);
@@ -48,9 +51,11 @@ while(true) {
 
         $data = trim($data);
 
+        //If there is data, proceed
         if (!empty($data)) {
              echo " send {$data}\n";
 
+             //Break down the message into parts : user_id, message, lat, lng
              $message = explode(";", $data);
 
              $package = [
@@ -88,12 +93,14 @@ while(true) {
                  echo "Out of range";
              }*/
 
+             //Add this information!
              $sql = "INSERT INTO messages (message, user_id) values (:message, :user_id)";
              $stmt = $db->prepare($sql);
              $stmt->execute($package);
-                echo "Clients: " . print_r($clients, true);
+             echo "Clients: " . print_r($clients, true);
              echo "Package: " . print_r($package, true);
 
+             //Respond
              foreach ($clients as $send_socket) {
                  if ($send_socket == $socket) {
                      continue;
@@ -105,4 +112,5 @@ while(true) {
      }
 }
 
+//We are done here
 socket_close($socket);
