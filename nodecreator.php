c9ed08fdd251ba002cb3a07d2eaf2a49e1dc0f3a
Mitchell Parker <mitchellparker@oakland.edu>
2019-04-02 22:59:14 -0400

    Adding a location creator

---
+++ nodecreator.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: SuxyShellSnarf
+ * Date: 2019-04-02
+ * Time: 22:56
+ */
+
+$coordinates[] = array(
+    array(
+        "lat_degrees" => "",
+        "lat_minutes" => "",
+        "lng_degrees" => "",
+        "lng_minutes" => "",
+    ), array(
+        "lat_degrees" => "",
+        "lat_minutes" => "",
+        "lng_degrees" => "",
+        "lng_minutes" => "",
+    ), array(
+        "lat_degrees" => "",
+        "lat_minutes" => "",
+        "lng_degrees" => "",
+        "lng_minutes" => "",
+    )
+);
+
+$coordinates[] = array(
+    array("lat_degrees" => "",
+        "lat_minutes" => "",
+        "lng_degrees" => "",
+        "lng_minutes" => "",
+    ), array(
+        "degrees" => "",
+        "minutes"
+    ), array(
+        "degrees" => "",
+        "minutes"
+    )
+);
+
+$coordinates[] = array(
+    array(
+        "degrees" => "",
+        "minutes"
+    ), array(
+        "degrees" => "",
+        "minutes"
+    ), array(
+        "degrees" => "",
+        "minutes"
+    )
+);
