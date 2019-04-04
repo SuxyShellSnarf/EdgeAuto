517bfb4531e322138a6eaa16f29f0450f30a644a
SuxyShellSnarf <mparker95@yahoo.com>
2019-04-04 18:35:13 -0400

    Link

--- sql/nodecreator.sql
+++ sql/nodecreator.sql
@@ -12,3 +12,4 @@ insert into location (x1y1, x1y2, x2y1, x2y2) values (1, 2, 4, 5);
 insert into location (x1y1, x1y2, x2y1, x2y2) values (2, 3, 5, 6);
 insert into location (x1y1, x1y2, x2y1, x2y2) values (4, 5, 7, 8);
 insert into location (x1y1, x1y2, x2y1, x2y2) values (5, 6, 8, 9);
+https://www.gps-coordinates.net/gps-coordinates-converter
