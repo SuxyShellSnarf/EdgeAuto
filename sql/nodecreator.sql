4612de84254f01e2b84abae15db4c94357a6ec12
SuxyShellSnarf <mparker95@yahoo.com>
2019-04-04 22:19:51 -0400

    Creating locations for school locations

--- sql/nodecreator.sql
+++ sql/nodecreator.sql
@@ -1,15 +1,4 @@
-insert into coordinate (x_degree, x_minutes, y_degree, y_minutes) values (42, 680000, -83, 220000);
-insert into coordinate (x_degree, x_minutes, y_degree, y_minutes) values (42, 680000, -83, 210000);
-insert into coordinate (x_degree, x_minutes, y_degree, y_minutes) values (42, 680000, -83, 200000);
-insert into coordinate (x_degree, x_minutes, y_degree, y_minutes) values (42, 670000, -83, 220000);
-insert into coordinate (x_degree, x_minutes, y_degree, y_minutes) values (42, 670000, -83, 210000);
-insert into coordinate (x_degree, x_minutes, y_degree, y_minutes) values (42, 670000, -83, 200000);
-insert into coordinate (x_degree, x_minutes, y_degree, y_minutes) values (42, 660000, -83, 220000);
-insert into coordinate (x_degree, x_minutes, y_degree, y_minutes) values (42, 660000, -83, 210000);
-insert into coordinate (x_degree, x_minutes, y_degree, y_minutes) values (42, 660000, -83, 200000);
-
-insert into location (x1y1, x1y2, x2y1, x2y2) values (1, 2, 4, 5);
-insert into location (x1y1, x1y2, x2y1, x2y2) values (2, 3, 5, 6);
-insert into location (x1y1, x1y2, x2y1, x2y2) values (4, 5, 7, 8);
-insert into location (x1y1, x1y2, x2y1, x2y2) values (5, 6, 8, 9);
-https://www.gps-coordinates.net/gps-coordinates-converter
+insert into location (uuperlat, lowerlat, upperlng, lowerlng) values (42.68, 42.674, -83.205, -83.2125);
+insert into location (uuperlat, lowerlat, upperlng, lowerlng) values (42.68, 42.674, -83.2125, -83.22)
+insert into location (uuperlat, lowerlat, upperlng, lowerlng) values (42.674, 42.668, -83.205, -83.2125);
+insert into location (uuperlat, lowerlat, upperlng, lowerlng) values (42.674, 42.668, -83.2125, -83.22);
