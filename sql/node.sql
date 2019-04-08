43321e600f6e0f2c7ebabf0fa397009e6c2dd3df
SuxyShellSnarf <mparker95@yahoo.com>
2019-04-07 22:05:50 -0400

    Data Collection

--- sql/node.sql
+++ sql/node.sql
@@ -11,9 +11,7 @@ arb_id int unsigned,
 message varchar(255),
 latitude float,
 longitude float,
-cantime varchar(255),
-user_id int unsigned,
-foreign key (user_id) references user(user_id)
+cantime bigint unsigned
 );
 
 -- This will act as a directory for vm lookup to switch easily between vms
