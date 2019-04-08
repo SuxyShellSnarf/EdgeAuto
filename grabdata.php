7c31eccdd8c1a5516d8ef81b3302684a2b31651b
SuxyShellSnarf <mparker95@yahoo.com>
2019-04-08 01:27:50 -0400

    Adding data grab

---
+++ grabdata.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: SuxyShellSnarf
+ * Date: 4/8/2019
+ * Time: 1:20 AM
+ */
+
+$db = new PDO("mysql:host=localhost;dbname=EdgeAuto", "edgeauto", "edgeauto19!");
+
+$sql = "select * from message where session_id = 1;";
+$stmt = $sql->prepare();
+$stmt->execute();
+$data = $stmt->fetch(PDO::FETCH_ASSOC);
+
+echo print_r($data, true);
