5c9e758b3df638ab3475924ee491545be33567ca
SuxyShellSnarf <mparker95@yahoo.com>
2019-04-08 01:31:46 -0400

    Fixing syntax

--- grabdata.php
+++ grabdata.php
@@ -9,7 +9,7 @@
 $db = new PDO("mysql:host=localhost;dbname=EdgeAuto", "edgeauto", "edgeauto19!");
 
 $sql = "select * from message where session_id = 1;";
-$stmt = $sql->prepare();
+$stmt = $db->prepare($sql);
 $stmt->execute();
 $data = $stmt->fetch(PDO::FETCH_ASSOC);
 
