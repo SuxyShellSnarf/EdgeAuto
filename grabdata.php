6ca0da8707f390e3432dd7d7271f30aaed6cb3e4
SuxyShellSnarf <mparker95@yahoo.com>
2019-04-08 01:42:19 -0400

    Fixing syntax

--- grabdata.php
+++ grabdata.php
@@ -8,9 +8,13 @@
 
 $db = new PDO("mysql:host=localhost;dbname=EdgeAuto", "edgeauto", "edgeauto19!");
 
+$file = "session1.txt";
+
 $sql = "select * from message where session_id = 1;";
 $stmt = $db->prepare($sql);
 $stmt->execute();
+$results = array();
 while($data = $stmt->fetch(PDO::FETCH_ASSOC)) {
-    echo print_r($data);
+    $string = $data["message_id"] . "," . $data["created"] . "," . $data["arb_id"] . "," . $data["message"] . "," . $data["latitude"] . "," . $data["longitude"] . "," . $data["cantime"] . "," . $data["session_id"];
+    fputcsv($file, $string);
 }
