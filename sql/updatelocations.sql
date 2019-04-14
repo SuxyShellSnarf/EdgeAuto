c46d4d3be91745009a451f5028240c0fc92eb619
SuxyShellSnarf <mparker95@yahoo.com>
2019-04-13 23:31:38 -0400

    Update locations

---
+++ sql/updatelocations.sql
@@ -0,0 +1,4 @@
+update location set upperlat = 42.845, lowerlat = 42.83, upperlng = -83.24, lowerlng = -83.3 where location_id = 1;
+update location set upperlat = 42.83, lowerlat = 42.815, upperlng = -83.24, lowerlng = -83.3 where location_id = 2;
+update location set upperlat = 42.815, lowerlat = 42.8, upperlng = -83.24, lowerlng = -83.3 where location_id = 3;
+update location set upperlat = 42.8, lowerlat = 42.785, upperlng = -83.24, lowerlng = -83.3 where location_id = 4;
